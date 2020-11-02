a5c5d364ea482d53fd27cb623a984ba25122f002
Michelle Jakab <mjakab@calpoly.edu>
2020-11-02 07:50:22 -0800

    Add Routing Logic
    Wrap the app in a react router so that we can switch what
    component we display depending on the url. Add a route for
    the previously added (though not completed) `Home` component.
    Now, when the path is exactly `/`, the `Home` component displays.
    Any other path doesn't display anything below the space reserved
    for the header.
    
    Also, make a couple corrections. Namely, move components.js out of the
    Home folder. I accidentally had put it there.

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import { Home } from './components/components'
 
 function App() {
     return (
         <div id="app">
-            Insert Header Here
-            <div id="page">
-                Insert Page Switching Logic Here
-                <Home />
-            </div>
+            Insert Nav Bar Here
+            <Switch>
+               <Route exact path='/'
+                 component={() => <Home />} />
+            </Switch>
         </div>
     );
 }
