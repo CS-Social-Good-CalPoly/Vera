f87e0e92895370dc1f1c40ed93ee5c95d7f596ab
irenna lumbuun <irenna.lumbuun@vw.com>
2020-11-22 15:16:11 -0800

    Add the component

---
+++ vera-react-app/src/components/StoryBanner/storyBanner.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './storyBanner.css'
+import { useHistory } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+function StoryBanner(props){
+    const history = useHistory();
+
+    const redirectToStory = () => {
+        let path = '/shareStory';
+        history.push(path);
+    }
+
+    return (
+        <Container id="story-banner">
+            <Row>
+                <Col xs={12} md={6} id="story-banner-img-col">
+                    <img src={props.imageUrl} alt="banner-image" id="story-banner-image"/>
+                </Col>
+                <Col xs={12} md={6} id="story-banner-text-col">
+                    <div id="story-banner-text">
+                        <h1 id="story-banner-text-title">Stories</h1>
+                        <p id="story-banner-text-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet aliquam justo. Morbi suscipit, massa quis tempus aliquam, tellus lectus consectetur lacus, ut iaculis mi ex posuere lorem. In vitae bibendum erat.</p>
+                        <button id="story-banner-button" onClick={redirectToStory}>Share Your Story</button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+/**
+ <img src={props.imageUrl} alt="banner-image" id="story-banner-image"></img>
+ */
+ /**
+
+  */
+
+export default StoryBanner;
