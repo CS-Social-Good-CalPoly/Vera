c73678b10b8986213e304e219953623bc5bb0709
Irenna Lumbuun <irennanicole1@gmail.com>
2021-01-13 21:16:27 -0800

    set fixed height for story banner

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { Home } from './components/components';
+import { Home, StoryBanner } from './components/components';
 
 function App() {
     return (
         <div id="app">
+            <StoryBanner imageUrl="https://dummyimage.com/300"/>
             <Switch>
                <Route exact path='/'
                  component={() => <Home />} />
