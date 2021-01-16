a546a79423ddccd6a909aa41dd0301cfa396516e
plandsman11 <parker.rey@gmail.com>
2021-01-16 12:28:51 -0800

    Updated Footer js to use appropriate tag

--- vera-react-app/src/components/Footer/Footer.js
+++ vera-react-app/src/components/Footer/Footer.js
@@ -4,15 +4,15 @@ import './Footer.css';
 function Footer() {
 
     return (
-        <div className="main-footer">
+        <footer className="main-footer">
             <div className="column1">
-                <h4>&copy; 2020 VERA</h4>
+                &copy; 2020 VERA
             </div>
             <div className="column3">
                 <a href="Recourses.html"> Resources </a> 
                 <a href="Stories.html"> Stories </a>
             </div>
-        </div>
+        </footer>
     );
 }
 
