250e4909026277e52786764d8c22dc5a20261213
Irenna Lumbuun <irennanicole1@gmail.com>
2021-01-24 16:11:03 -0800

    remove story banner from app.js

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { Home, StoryBanner } from './components/components';
+import { Home } from './components/components';
 
 function App() {
     return (
         <div id="app">
-            <StoryBanner imageUrl="https://dummyimage.com/300"/>
             <Switch>
                <Route exact path='/'
                  component={() => <Home />} />
