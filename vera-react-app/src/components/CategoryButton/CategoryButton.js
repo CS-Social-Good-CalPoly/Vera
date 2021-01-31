5ba5104b365477c446b21cbc26d67e626e4bf97d
Nick Firme <nickfirme@live.com>
2021-01-31 11:04:54 -0800

    Added CategoryButton component

---
+++ vera-react-app/src/components/CategoryButton/CategoryButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './CategoryButton.css'
+
+function CategoryButton(props) {
+
+    function scrollToElement() {
+        const element =  document.getElementById(props.location);
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    return (
+        <button className="CategoryButton" onClick={scrollToElement}>
+        <span className="button-text">{props.category}</span>
+        </button>
+    )
+
+}
+
+export default CategoryButton;
