81b00918186bfb5c3d10b2cb3e44d1a1a945265c
Irenna Nicole Lumbuun <irennanicole@yahoo.com>
2021-01-31 00:36:50 -0800

    Update vera-react-app/src/App.js
    Co-authored-by: Michelle <38011503+Michelle5102@users.noreply.github.com>

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, Footer} from './components/components';
 
 function App() {
-    const toExpect = ["Open to all cal poly students for walk in services", "Bring a backpack or reusable bags to carry food", "Assitance from a volunteer and information on additional resources"]
     return (
         <div id="app">
             <Switch>
