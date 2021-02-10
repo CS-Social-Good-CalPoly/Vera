cf1e956dee6f7fa365b2bc78164b47f42f5e000a
Irenna Lumbuun <irennanicole1@gmail.com>
2021-02-10 14:10:19 -0800

    restore app.js

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { Home, Footer, StoryBanner} from './components/components';
+import { Home, Footer} from './components/components';
 
 function App() {
     return (
         <div id="app">
-            <StoryBanner imageUrl={"https://www.picsum.photos/300"}/>
             <Switch>
                <Route exact path='/'
                  component={() => <Home />} />
