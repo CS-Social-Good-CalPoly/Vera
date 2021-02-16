90c8efbea8b022483b04d041b750845dbfec94a3
plandsman11 <parker.rey@gmail.com>
2021-02-15 18:04:14 -0800

    Added toggle functionality to hamburger menu and adjusted font

--- vera-react-app/src/components/Navbar/Navbar.js
+++ vera-react-app/src/components/Navbar/Navbar.js
@@ -10,9 +10,10 @@ function Navbar() {
                 <img id="logo" src={logo} height="35" width="35" ></img>
             </div>
             <div className="optionCol">
-                <div id="mobile"> 
+                <button class="mobile" id= "mobile" onClick={() =>
+                         {document.getElementById('desktop').classList.toggle('show')}  }> 
                     <img id="option" src={optionBar} height="16" width="16" ></img>
-                </div>
+                </button>
                 <div id="desktop">
                     <a id ="link" href="Recourses.html"> Resources </a> 
                     <a id ="link" href="Stories.html"> Stories </a>
