2bad84030a2ebb9617f3a1f5667306e84dfde666
Nick Firme <nickfirme@live.com>
2021-02-21 14:09:57 -0800

    Added StoryTile to components file

--- vera-react-app/src/components/components.js
+++ vera-react-app/src/components/components.js
@@ -1,2 +1,4 @@
 export {default as Home} from './Home/Home';
 export {default as Banner} from './Banner/Banner';
+export {default as CategoryButton} from './CategoryButton/CategoryButton'
+export {default as StoryTile} from './StoryTile/StoryTile'
