9d1a6f4b797a6392d01e6a511d0a60946001be1f
Anushree Parmar <anuaparmar@gmail.com>
2021-03-01 04:20:33 -0800

    Fixed left and right margins and overflow of buttons

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -3,8 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, Footer, CategoryButtonGroup} from './components/components';
 
 function App() {
-    const locations = ["this", "that", "this this"];
-    const name = ["name1", "name2", "name3"];
+    const locations = ["this", "that", "this this", "that that", "this that", "that this", "t", "h", "i", "s"];
+    const name = ["name1", "name2", "name3", "name4", "name5", "name6", "name7", "name8", "name9", "name10"];
     return (
         <div id="app">
             <Switch>
