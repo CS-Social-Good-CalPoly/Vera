7a15e2f5b2fbbbd6ae5f4d30f5ec12e70fe003d6
Irenna Lumbuun <irennanicole1@gmail.com>
2021-03-01 22:16:47 -0800

    remove from app.js

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -3,9 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, Footer, ResourceTile} from './components/components';
 
 function App() {
-    const toExpect1 = ["the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog.", "two", "three four five six"]
-    const toExpect = ["One", "two", "three"]
-    const openHours = ["Monday - Friday 8:00am - 5:00pm", "Saturday - Sunday 10:00am - 3:00pm"]
     return (
         <div id="app">
             <Switch>
