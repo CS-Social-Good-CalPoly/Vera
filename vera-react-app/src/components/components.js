3fbe680c0f43302743ab0f4f0bb190bf5f6eb0e9
Anushree Parmar <anuaparmar@gmail.com>
2021-03-24 21:59:54 -0700

    Standardized Text Styling

--- vera-react-app/src/components/components.js
+++ vera-react-app/src/components/components.js
@@ -3,4 +3,3 @@ export {default as Footer} from './Footer/Footer';
 export {default as StoryBanner} from './StoryBanner/storyBanner';
 export {default as Banner} from './Banner/Banner';
 export {default as CategoryButton} from './CategoryButton/CategoryButton';
-export {default as CategoryButtonGroup} from './CategoryButtonGroup/CategoryButtonGroup';
