9ebdcec9d61b23801d8b43a0ddddea984269a74a
Nick Firme <nickfirme@live.com>
2021-04-01 18:53:20 -0700

    Added ResoucePageTileGroup to components export file

--- vera-react-app/src/components/components.js
+++ vera-react-app/src/components/components.js
@@ -3,3 +3,5 @@ export {default as Footer} from './Footer/Footer';
 export {default as StoryBanner} from './StoryBanner/storyBanner';
 export {default as Banner} from './Banner/Banner';
 export {default as CategoryButton} from './CategoryButton/CategoryButton';
+export {default as ResourcePageTileGroup} from './ResourcePageTileGroup/ResourcePageTileGroup'
+
