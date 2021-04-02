18cdfee8cb0c30622d3f7a113db4ef5549c0b77a
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-04-01 18:28:34 -0700

    Made changes: header is now super long to end of page, mobile header is black instead of randomly being blue, left aligned titles with its header, changed desktop and mobile tiles to have better margins and spacing. Check issue's comments on generating test data.

--- vera-react-app/src/App.js
+++ vera-react-app/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
-import {Home, Footer} from './components/components';
+import { Route, Switch } from 'react-router-dom';
+import { Home, Footer } from './components/components';
 
 function App() {
     return (
         <div id="app">
             <Switch>
-                <Route exact path='/'
-                       component={() => <Home/>}/>
+                <Route exact path='/' component={() => <Home/>}/>
             </Switch>
             <Footer/>
         </div>
