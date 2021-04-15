f818903e9bdfec7076b72b30820a731b143ce91b
Nick Firme <nickfirme@live.com>
2021-04-14 18:42:45 -0700

    Prettied up the ResoucePageTileGroup component file

--- vera-react-app/src/components/ResourcePageTileGroup/ResourcePageTileGroup.js
+++ vera-react-app/src/components/ResourcePageTileGroup/ResourcePageTileGroup.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { ResourcePageTileGroupDiv, TitleContainer, Heading, TileGroup, Tiles } from '../Shared/ResourcePageTileGroup'
+import {
+    ResourcePageTileGroupDiv,
+    TitleContainer,
+    Heading,
+    TileGroup,
+    Tiles,
+} from '../Shared/ResourcePageTileGroup'
 
-function ResourcePageTileGroup({id, title, resources}) {
-
+function ResourcePageTileGroup({ id, title, resources }) {
     return (
         <ResourcePageTileGroupDiv>
             <TitleContainer>
                 <Heading id={id}>{title}</Heading>
             </TitleContainer>
             <TileGroup>
-                {
-                    resources.map(
-                        (resource, index) => (
-                            <Tiles key={index}>
-                                <p>{resource.id}</p>
-                                <p>{resource.title}</p>
-                                <p>{resource.imageUrl}</p>
-                            </Tiles>
-                        )
-                    )
-                }
+                {resources.map((resource, index) => (
+                    <Tiles key={index}>
+                        <p>{resource.id}</p>
+                        <p>{resource.title}</p>
+                        <p>{resource.imageUrl}</p>
+                    </Tiles>
+                ))}
             </TileGroup>
         </ResourcePageTileGroupDiv>
-    )
+    );
 }
 
 export default ResourcePageTileGroup;
