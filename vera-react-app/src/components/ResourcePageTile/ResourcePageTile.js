013dd4e31dcdbd67a4dc3f29d2e1bfba398c39d4
plandsman11 <parker.rey@gmail.com>
2021-04-14 19:22:58 -0700

    Making progress with clamps

--- vera-react-app/src/components/ResourcePageTile/ResourcePageTile.js
+++ vera-react-app/src/components/ResourcePageTile/ResourcePageTile.js
@@ -3,6 +3,7 @@ import './ResourcePageTile.css';
 import arrowPic from './images/Arrow.jpg';
 import bannerPic from "./images/TesterImage.jpg"
 import { Link } from "react-router-dom";
+import Dotdotdot from 'react-clamp';
 
 /* Component Props
  * imageUrl
@@ -13,14 +14,20 @@ import { Link } from "react-router-dom";
 
 function ResourcePageTile(props) {
 
+  
+
     return (
         <div className="ResourcePageTileWrapper">
           <Link to={props.resourcePageLink}>
             <div className="ResourcePageTile" >
               <img className="banner" src={bannerPic} alt={bannerPic}/>
               <div className="content-box">
-                <h1 className="title">{props.title} </h1>
-                <h2 className="info-text">{props.info}</h2>
+              <Dotdotdot clamp={2} className="three-line-text-clamp">
+                  <h1 className="title">{props.title} </h1>
+                </Dotdotdot>
+                <Dotdotdot clamp={3} className="three-line-text-clamp">
+                  <h2 className="info-text">{props.info}</h2>
+                </Dotdotdot>
               </div>
               <img className="icon" src={arrowPic} height="20px" width="20px" alt="" />
             </div>
