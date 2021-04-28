1969c29e2d4c0f1bf0acf2ef5a10139ac8bc6bda
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-04-28 14:51:12 -0700

    Added mobile css changes to take display:none; made mobile css look prettier.

--- vera-react-app/src/components/StoryPopUp/StoryPopUp.js
+++ vera-react-app/src/components/StoryPopUp/StoryPopUp.js
@@ -4,32 +4,19 @@ import {Card} from 'react-bootstrap';
 
 function StoryPopUp(props) {
 
-    /*
-
-     */
-
-    {/*{(window.innerWidth < 768) ? <Card.Title className='title'>Family</Card.Title> : <Card.Title className='title'> How My Dog Helps me Through College</Card.Title>}*/
-    }
-
     return (
-        <Card style={{
-            maxWidth: '60%',
-            margin: 'auto',
-            marginTop: '10vh',
-            fontFamily: 'Poppins',
-            color: '#4A6E82'
-        }}>
+        <Card className='width-wrapper'>
             {/*<Card.Img variant="top" src={props.image}/>*/}
             <Card.Img style={{maxHeight: '250px'}} variant="top" src="https://i.pinimg.com/originals/b1/d6/b4/b1d6b4715bdb30d7b7f3253f2423e327.jpg"/>
             <Card.Body>
                 <div className='header'>
+                    <Card.Title style={{fontWeight: '600', paddingLeft: '10px'}} className='mobile'> How My Dog Helps me Through College</Card.Title>
                     <div className='info'>
                         <div>
                             <Card.Title className='desktop' style={{fontWeight: '600'}}>Family</Card.Title>
                             <Card.Subtitle id='year'>4th Year</Card.Subtitle>
                             <Card.Subtitle id='major'>Chemistry Major</Card.Subtitle>
                         </div>
-                        <Card.Title className='mobile'> How My Dog Helps me Through College</Card.Title>
                         <Card.Text id='date'>Dec 11, 2020</Card.Text>
                     </div>
                 </div>
