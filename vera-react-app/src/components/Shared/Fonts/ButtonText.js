25b1e6015b48694fb7de31638f36ddc173e94d97
Adelle Vo <adelle.vo@gmail.com>
2021-05-26 16:08:44 -0700

    font styled components

---
+++ vera-react-app/src/components/Shared/Fonts/ButtonText.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonText = styled.p `
+    font-family: Poppins;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.05em;
+    text-align: left;
+
+    @media only screen and (max-width: 768px) {
+        font-size: 14px;
+        line-height: 21px;
+    }
+`;
