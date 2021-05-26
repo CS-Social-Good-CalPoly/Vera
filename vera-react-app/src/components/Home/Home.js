e4be0d134242b7b2d8f4680ca7c180f9de8e121c
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-05-26 16:49:00 -0700

    Changed dummy to test out the scrolling feature.

--- vera-react-app/src/components/Home/Home.js
+++ vera-react-app/src/components/Home/Home.js
@@ -3,15 +3,16 @@ import './Home.css';
 import { Banner, CategoryButtonGroup, ResourcePageTileGroup} from '../components'
 
 function Home() {
-    const categorNames = ['first', 'second']
-    const categorLocs = ['here', 'there']
-    const resourceDummies = [ { id: 'general-stress', title: 'General Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, ]
-
+    const categorNames = ['Support', 'Stress']
+    const categorLocs = ['Support', 'Stress']
+    const resourceSupport = [ { id: 'general-stress', title: 'General Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, ]
+    const resourceStress = [ { id: 'general-stress', title: 'General Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, ]
     return (
         <div>
             <Banner imageUrl='https://pyxis.nymag.com/v1/imgs/3d4/0aa/89125115b0e10b94e3378d484712450727-25-thanos.rsquare.w1200.jpg' />
             <CategoryButtonGroup title='Categories' names={categorNames} locations={categorLocs}/>
-            <ResourcePageTileGroup id="Stress" title="Support" resources={resourceDummies} />
+            <ResourcePageTileGroup id="Support" title="Support" resources={resourceSupport} />
+            <ResourcePageTileGroup id="Stress" title="Stress" resources={resourceStress} />
         </div>
     );
 }
