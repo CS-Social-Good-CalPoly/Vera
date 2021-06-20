c528fd9703e2d55a92db0af613eaa0f1ac07a398
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-06-20 12:23:12 -0700

    Added /subrsrcs get route. Added models for SubResources.js.

--- backend/models/SubResources.js
+++ backend/models/SubResources.js
@@ -3,7 +3,17 @@ const {model, Schema} = require('mongoose')
 // incomplete
 const SubResourceSchema = new Schema({
     Name: String,
-    Title: String
+    Title: String,
+    ShortDescription: String,
+    LongDescription: String,
+    ImageUrl: String,
+    ImageAltText: String,
+    ResourceIDList: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'individual-resources'
+        }
+    ]
 })
 
-module.exports = model('resource-sub-category', SubResourceSchema)
+module.exports = model('resource-sub-category', SubResourceSchema, 'resource-sub-category')
--- backend/routes/resources.js
+++ backend/routes/resources.js
@@ -3,9 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 const GenResources = require('../models/GenResources')
+const SubResources = require('../models/SubResources')
 
-router.get('/generalrsrcs', async (req, res) => {
-    // res.send('General Resources here')
+router.get('/generalrsrcscat', async (req, res) => {
     try {
         const rsrc = await GenResources.find()
         res.json(rsrc)
@@ -15,7 +15,12 @@ router.get('/generalrsrcs', async (req, res) => {
 })
 
 router.get('/subrsrcs', async (req, res) => {
-    res.send('Sub Resources here')
+    try {
+        const rsrc = await SubResources.find()
+        res.json(rsrc)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
 module.exports = router
