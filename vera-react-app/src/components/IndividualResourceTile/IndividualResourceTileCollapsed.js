caca8c049fecbfcb9e2870eabd6cedcd21c24835
Nick Firme <nickfirme@live.com>
2022-02-12 13:49:16 -0800

    Modify collapsed resource tiles to use shared TileGroup components

--- vera-react-app/src/components/IndividualResourceTile/IndividualResourceTileCollapsed.js
+++ vera-react-app/src/components/IndividualResourceTile/IndividualResourceTileCollapsed.js
@@ -1,42 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import {
-    Tile,
-    TileBanner,
-    TileIcon,
-    TileTitle
-} from '../Shared/Tile'
-import arrowIcon from '../Shared/arrow-icon.svg'
+import React from 'react';
+import styled from 'styled-components';
+import { Tiles, Banner } from '../Shared/TileGroup';
+import { TileIcon, TileTitle } from '../Shared/Tile';
+import arrowIcon from '../Shared/arrow-icon.svg';
 
 const InfoText = styled.p`
-    font-family: Poppins;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 26px;
-    letter-spacing: 0.05em;
-    color: #4A6E82;
-    padding: 0 22px 22px;
-    position: relative;
-    bottom: 10px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 26px;
+  letter-spacing: 0.05em;
+  color: #4a6e82;
+  padding: 0 22px 22px;
+  position: relative;
+  bottom: 10px;
 
-    @media only screen and (max-width: 768px) {
-        font-size: 12px;
-        line-height: 12px;
-        padding: 0 6px 12px;
-    }
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+    line-height: 12px;
+    padding: 0 6px 12px;
+  }
 `;
 
 function IndividualResourceTileCollapsed(props) {
-    return (
-        <Tile onClick={props.handleChange}>
-            <TileBanner src={props.imageUrl} alt={props.title} />
-            <TileTitle>{props.title}</TileTitle>
-            <InfoText>{props.infoText}</InfoText>
-            <TileIcon src={arrowIcon} />
-        </Tile>
-    )
+  return (
+    <Tiles onClick={props.handleChange}>
+      <Banner src={props.imageUrl} alt={props.title} />
+      <TileTitle>{props.title}</TileTitle>
+      <InfoText>{props.infoText}</InfoText>
+      <TileIcon src={arrowIcon} />
+    </Tiles>
+  );
 }
 
 export default IndividualResourceTileCollapsed;
-
