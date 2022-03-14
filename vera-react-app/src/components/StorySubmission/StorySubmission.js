65eb06bf11d2c6cc58fdfad468245f62237d6206
plandsman11 <parker.rey@gmail.com>
2022-03-13 17:35:20 -0700

    Added some styling on the submission box

--- vera-react-app/src/components/StorySubmission/StorySubmission.js
+++ vera-react-app/src/components/StorySubmission/StorySubmission.js
@@ -3,6 +3,9 @@ import { StoryBanner, DropDownForm } from '../components';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './StorySubmission.css';
+import {
+  PageTitle,
+} from '../Shared/Fonts/PageTitle'
 
 function StorySubmission() {
   const body_text = "There’s a ton of ways to share your story. The most important thing is that you feel safe and comfortable with however you choose to do it. If you’re nervous, try to think beforehand about what you’d like to say, or bang out a draft before you hit ‘post’. If you feel the need to get personal stuff off your chest, find someone you trust to share it with and give yourself as much time as you need.";
@@ -13,10 +16,19 @@ function StorySubmission() {
   return (
       <div>
           <StoryBanner bodyText={body_text} imageUrl={image_url}/>
-          <DropDownForm fieldTitle="Year"/>
-          <DropDownForm fieldTitle="College"/>
-          <DropDownForm fieldTitle="Major"/>
-          <ReactQuill theme="snow" value={quillValue} onChange={setQuillValue}/>          
+          <div className="background">
+            <div className="story-submission-box">
+              <DropDownForm fieldTitle="Year"/>
+              <DropDownForm fieldTitle="College"/>
+              <DropDownForm fieldTitle="Major"/>
+              <div className="description-box"> 
+                <div className="title-text">
+                  Short Description
+                </div>
+                <ReactQuill theme="snow" value={quillValue} onChange={setQuillValue}/> 
+              </div> 
+            </div>        
+          </div>
       </div>
   );
 }
