5f0d98baad4a66570656232d000756f9e0621186
plandsman11 <parker.rey@gmail.com>
2023-01-16 13:14:13 -0800

    Removed testing files, ready for merge

--- vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourcePage.js
+++ vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourcePage.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Banner, IndividualResourceTileGroup } from '../components';
 
 function IndividualResourcePage() {
-  const resources = [
-    {
+
+  const resource = {
       imageUrl:
         'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
       title: 'title',
@@ -14,248 +14,25 @@ function IndividualResourcePage() {
       phone: '123456789',
       hourList: ['hour1', 'hour2'],
       link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-  ];
-  const resources2 = [
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    }
-  ];
-  
-  const resources4 = [
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    }
-    
-  ];
-  const resources1 = [
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    }
-  ];
-  const resourcesMultipleRows = [
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      descriton: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    }
-    
-  ];
+  };
+
   return (
     <div>
       <Banner imageUrl="https://pyxis.nymag.com/v1/imgs/3d4/0aa/89125115b0e10b94e3378d484712450727-25-thanos.rsquare.w1200.jpg" />
       <IndividualResourceTileGroup
         id="School"
         title="School"
-        resources={resources}
+        resources={[resource, resource, resource]}
       />
       <IndividualResourceTileGroup
         id="Community"
-        title="Comunity"
-        resources={resources}
+        title="Community"
+        resources={[resource, resource, resource, resource]}
       />
       <IndividualResourceTileGroup
-        id="Testing 1 box"
-        title="Testing"
-        resources={resources1}
-      />
-      <IndividualResourceTileGroup
-        id="Testing 2 boxes"
-        title="Testing"
-        resources={resources2}
-      />
-      <IndividualResourceTileGroup
-        id="Testing 3 boxes"
-        title="Testing"
-        resources={resources}
-      />
-      <IndividualResourceTileGroup
-        id="Testing 4 boxes"
-        title="Testing"
-        resources={resources4}
-      />
-      <IndividualResourceTileGroup
-        id="Testing 4 boxes"
-        title="Testing"
-        resources={resourcesMultipleRows}
+        id="National"
+        title="National"
+        resources={[resource, resource, resource, resource, resource]}
       />
     </div>
   );
