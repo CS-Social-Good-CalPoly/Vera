22dc25797b204483106e82a3e4acd0b708a64192
plandsman11 <59515892+plandsman11@users.noreply.github.com>
2023-01-16 13:31:59 -0800

    Swap Tile Group to Grid (#87)
    * test
    
    * same row three columns
    
    * Testing for grid
    
    * Testing page
    
    * Minor changes
    
    * Modified the margins
    
    * Fixed minor updates
    
    * Minor fixes
    
    * Removed testing files, ready for merge
    
    Co-authored-by: Daiwik Swaminathan <daiwik.swaminathan@gmail.com>

--- vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourcePage.js
+++ vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourcePage.js
@@ -2,56 +2,37 @@ import React from 'react';
 import { Banner, IndividualResourceTileGroup } from '../components';
 
 function IndividualResourcePage() {
-  const resources = [
-    {
+
+  const resource = {
       imageUrl:
         'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
       title: 'title',
-      description: 'This is test description of this component. Click to expand!',
+      descriton: 'description',
       buildingName: 'buildingName',
       address: 'address',
       whatToExpectList: ['thing1', 'thing2'],
       phone: '123456789',
       hourList: ['hour1', 'hour2'],
       link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      description: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-    {
-      imageUrl:
-        'https://www.onceuponachef.com/images/2019/09/Spaghetti-and-Meatballs.jpg',
-      title: 'title',
-      description: 'description',
-      buildingName: 'buildingName',
-      address: 'address',
-      whatToExpectList: ['thing1', 'thing2'],
-      phone: '123456789',
-      hourList: ['hour1', 'hour2'],
-      link: 'link',
-    },
-  ];
+  };
+
   return (
     <div>
       <Banner imageUrl="https://pyxis.nymag.com/v1/imgs/3d4/0aa/89125115b0e10b94e3378d484712450727-25-thanos.rsquare.w1200.jpg" />
       <IndividualResourceTileGroup
         id="School"
         title="School"
-        resources={resources}
+        resources={[resource, resource, resource]}
       />
       <IndividualResourceTileGroup
         id="Community"
-        title="Comunity"
-        resources={resources}
+        title="Community"
+        resources={[resource, resource, resource, resource]}
+      />
+      <IndividualResourceTileGroup
+        id="National"
+        title="National"
+        resources={[resource, resource, resource, resource, resource]}
       />
     </div>
 
