2b1653fa9b33a1be66a8cc83412dfc200190403e
Laura <lcamac02@calpoly.edu>
2023-01-27 23:05:40 -0800

    uhh

--- vera-react-app/src/pages/HomePage/HomePage.js
+++ vera-react-app/src/pages/HomePage/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
     const resourceStress = [ { id: 'general-stress', title: 'General Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, ]
     return (
         <div>
-            <Banner imageUrl='https://pyxis.nymag.com/v1/imgs/3d4/0aa/89125115b0e10b94e3378d484712450727-25-thanos.rsquare.w1200.jpg' />
+            <Banner imageUrl= "/static/media/VeraLogo.6054b70a.jpg"/>
             <CategoryButtonGroup title='Categories' names={categorNames} locations={categorLocs}/>
             <ResourcePageTileGroup id="Support" title="Support" resources={resourceSupport} />
             <ResourcePageTileGroup id="Stress" title="Stress" resources={resourceStress} />
