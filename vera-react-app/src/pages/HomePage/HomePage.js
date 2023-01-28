cbeba899a058e42cef9ca2e18bb14e15ab79e0d4
Laura <lcamac02@calpoly.edu>
2023-01-28 09:46:29 -0800

    hmm

--- vera-react-app/src/pages/HomePage/HomePage.js
+++ vera-react-app/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.css';
+import bg from '../../components/Banner/bannerBackground.jpg';
 import { Banner, CategoryButtonGroup, ResourcePageTileGroup} from '../../components/components'
 
 function HomePage() {
@@ -9,7 +10,7 @@ function HomePage() {
     const resourceStress = [ { id: 'general-stress', title: 'General Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, { id: 'chronic-stress', title: 'Chronic Stress', imageUrl: '----------insert an image url here -----------' }, ]
     return (
         <div>
-            <Banner imageUrl= "/static/media/VeraLogo.6054b70a.jpg"/>
+            <Banner imageUrl= {bg} words = "bruh"/>
             <CategoryButtonGroup title='Categories' names={categorNames} locations={categorLocs}/>
             <ResourcePageTileGroup id="Support" title="Support" resources={resourceSupport} />
             <ResourcePageTileGroup id="Stress" title="Stress" resources={resourceStress} />
