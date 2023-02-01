d1f53c86b95355c027873a89d017875cd2dfd635
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2023-02-01 14:11:17 -0800

    added padding to make spacing better, safari not ideal

--- vera-react-app/src/components/CategoryButton/CategoryButton.js
+++ vera-react-app/src/components/CategoryButton/CategoryButton.js
@@ -7,8 +7,10 @@ const {Link} = Anchor
 
 function CategoryButton(props) {
 
-    // Only issue with this is that offset seems to not be possible
-    // If this can be figured out, this would be an optimal solution...
+    // Only issue with this solution is that
+    // scrollIntoView is not optimized for
+    // safari. It still works, but it is not
+    // as smooth as chrome.
     function scrollToElement() {
         const element =  document.getElementById(props.location);
         element.scrollIntoView({ behavior: 'smooth'})
@@ -16,22 +18,21 @@ function CategoryButton(props) {
 
     return (
         <button className="CategoryButton" onClick={scrollToElement}>
-            {/* <span className="button-text">{props.category}</span> */}
-            <Anchor targetOffset='45'>
-                <Link href={'#' + props.className} title={props.category} />
-            </Anchor>
+            <span className="button-text">{props.category}</span>
         </button>
 
         // Notes about using Anchor:
         // - Link: https://www.youtube.com/watch?v=7mZ4yguae9Q
         // - You might need to download (use this command in terminal): npm i antd
         // - You can easily move between sections
-        // - Styling looks bad
+        // - Styling is not correct
         // - Offset exists, but it might differ between mobile and desktop
 
-        // <Anchor targetOffset='15'>
-        //     <Link href={'#'+props.location} title={props.category}/>
-        // </Anchor>
+        // <button className="CategoryButton">
+        //     {/* <Anchor targetOffset='45'> */}
+        //         <Link href={'#'+props.location} title={props.category}/>
+        //     {/* </Anchor> */}
+        // </button>
     )
 
 }
