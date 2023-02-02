65017b4508ec990ed1d59608e5df82b82ffa915e
daiwik-swaminathan <60561821+daiwik-swaminathan@users.noreply.github.com>
2023-02-01 16:41:29 -0800

    Add skip to section scroll effect (#103)
    * added padding to make spacing better, safari not ideal
    
    * retrying last push
    
    * Deleted comments and cleaned up branch
    
    We removed comments and code related to the unused Anchor method.
    
    ---------
    
    Co-authored-by: Ricardo Beltran <45570556+RBeltran12358@users.noreply.github.com>

--- vera-react-app/src/components/CategoryButton/CategoryButton.js
+++ vera-react-app/src/components/CategoryButton/CategoryButton.js
@@ -1,37 +1,17 @@
 import React from 'react'
 import './CategoryButton.css'
 
-import { Anchor } from 'antd'
-
-const {Link} = Anchor
-
 function CategoryButton(props) {
 
-    // Only issue with this is that offset seems to not be possible
-    // If this can be figured out, this would be an optimal solution...
     function scrollToElement() {
-        const element =  document.getElementById(props.location);
+        const element =  document.getElementById(props.location); 
         element.scrollIntoView({ behavior: 'smooth'})
     }
 
     return (
         <button className="CategoryButton" onClick={scrollToElement}>
-            {/* <span className="button-text">{props.category}</span> */}
-            <Anchor targetOffset='45'>
-                <Link href={'#' + props.className} title={props.category} />
-            </Anchor>
+            <span className="button-text">{props.category}</span>
         </button>
-
-        // Notes about using Anchor:
-        // - Link: https://www.youtube.com/watch?v=7mZ4yguae9Q
-        // - You might need to download (use this command in terminal): npm i antd
-        // - You can easily move between sections
-        // - Styling looks bad
-        // - Offset exists, but it might differ between mobile and desktop
-
-        // <Anchor targetOffset='15'>
-        //     <Link href={'#'+props.location} title={props.category}/>
-        // </Anchor>
     )
 
 }
