d0a9d426b44ed1e3616f3d87ca7dbcf5331f956e
Laura <lcamac02@calpoly.edu>
2023-02-04 23:00:36 -0800

    mhmm

--- vera-react-app/src/components/Banner/Banner.js
+++ vera-react-app/src/components/Banner/Banner.js
@@ -13,8 +13,7 @@ function Banner(props) {
         <h2 className="header2">{props.tagline1}</h2>
         <h2 className="header3">{props.tagline2}</h2>
         <picture className="logo">
-        <source media="(min-width: 0px)" srcSet={props.logo} />
-        <img src={props.logo} alt="vera logo" />
+        <img className="logo2" src={props.logo} alt="vera logo" />
       </picture>
       </div>
     );
