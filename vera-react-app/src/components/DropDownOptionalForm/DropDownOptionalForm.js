723679261c0db894a08a5041472e0cd722bb1c4e
eliseemelia <eliseemelia@gmail.com>
2023-02-20 21:52:40 -0800

    tried adding state

---
+++ vera-react-app/src/components/DropDownOptionalForm/DropDownOptionalForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Component } from "react";
+import './DropDownOptionalForm.css';
+
+class DropDownOptionalForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: ""
+    };
+
+    this.handleDropdownChange = this.handleDropdownChange.bind(this);
+  }
+
+  handleDropdownChange(e) {
+    this.setState({ selectValue: e.target.value });
+  }
+
+  render() {
+    return (
+          <div className="wrapper-drop-down">
+            <select id="dropdown-optional" onChange={this.handleDropdownChange}>
+              <option value="N/A" id="options">{this.props.fieldTitle}</option>
+              {this.props.myoptions.map((optionTitle) => (
+                 <option value="1" id="options">{optionTitle}</option>
+              ))}
+            </select>
+          </div>
+    );
+  }
+}
+
+
+
+export default DropDownOptionalForm;
