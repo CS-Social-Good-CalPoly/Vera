599ac17240a247e933e718470e7ae1679691bff8
plandsman11 <parker.rey@gmail.com>
2023-02-22 16:52:53 -0800

    Styling updates and simplification

--- vera-react-app/src/components/Banner/Banner.js
+++ vera-react-app/src/components/Banner/Banner.js
@@ -13,9 +13,7 @@ function Banner(props) {
           <h2 className="header2">{props.tagline1}</h2>
           <h2 className="header3">{props.tagline2}</h2>
         </div>
-        <picture className="logo">
-          <img className="logo2" src={props.logo} alt="vera logo" />
-        </picture>
+        <img className="logo" src={props.logo} alt="vera logo" />
       </div>
     );
 }
