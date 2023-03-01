0d4c21054ae7a3d0dbbdcbefa30b9409cc4d3cf9
Abigayle Mercer <67762825+Abigayle-Mercer@users.noreply.github.com>
2023-03-01 15:31:18 -0800

    Added Text Box component (#137)
    * "added text block component"
    
    * added TextBlock component
    
    * added text box
    
    * Combining Adi and Abigayle's changes
    
    ---------
    
    Co-authored-by: plandsman11 <parker.rey@gmail.com>

---
+++ vera-react-app/src/components/TextBlock/TextBlock.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TextBlock.css";
+
+function TextBlock(props) {
+
+
+    return <div className="TextBox">  
+        <p className="Text">
+            {props.text}
+        </p>
+    </div>
+
+ }
+
+
+ export default TextBlock;
--- vera-react-app/src/pages/IndividualResourcePage/IndividualResourcePage.js
+++ vera-react-app/src/pages/IndividualResourcePage/IndividualResourcePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Banner,
   IndividualResourceTileGroup,
-  CategoryButtonGroup,
+  CategoryButtonGroup, TextBlock,
 } from "../../components/components";
 
 function IndividualResourcePage() {
@@ -23,9 +23,23 @@ function IndividualResourcePage() {
       link: 'link',
   };
 
+  const resource2 = {
+    imageUrl:
+      "https://bestlifeonline.com/wp-content/uploads/sites/3/2019/04/weird-dog-lizard-hybrid.jpg?quality=82&strip=1&resize=640%2C360",
+    title: "title",
+    description: "description",
+    buildingName: "buildingName",
+    address: "address",
+    whatToExpectList: ["thing1", "thing2"],
+    phone: "123456789",
+    hourList: ["hour1", "hour2"],
+    link: "link",
+  };
+
   return (
     <div>
       <Banner imageUrl="https://cdn.pixabay.com/photo/2017/03/25/03/29/cherry-tomatoes-2172700_1280.jpg" />
+
 
       <CategoryButtonGroup
         title="FOOD INSECURITY RESOURCES"
@@ -33,15 +47,17 @@ function IndividualResourcePage() {
         locations={categorLocs}
       />
 
+      <TextBlock text = {"For many students today, food insecurity is just a few missed paychecks away. A 2018 study by found that 36% of college students are experienceing hunger and lack of stable housing. Add in the fact that tuition rates are going up while financial aid is going down, and it’s obvious that most college students and their families are feeling a very tight financial squeeze. But there is help out there. Students struggling to avoid hunger can find several resources to put food on the table while still completing their education."}/>
+
       <IndividualResourceTileGroup
         id="School"
         title="School"
-        resources={[resource, resource, resource]}
+        resources={[resource2, resource, resource]}
       />
       <IndividualResourceTileGroup
         id="Community"
         title="Community"
-        resources={[resource, resource, resource, resource]}
+        resources={[resource, resource, resource2, resource]}
       />
       <IndividualResourceTileGroup
         id="National"
