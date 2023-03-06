27b2a61a07d0a22d5efa345b31111ee11347e755
Aditya Manikonda <adityamanikonda123@gmail.com>
2023-03-06 01:26:55 -0800

    dealt with merging issues

--- vera-react-app/src/pages/IndividualResourcePage/IndividualResourcePage.js
+++ vera-react-app/src/pages/IndividualResourcePage/IndividualResourcePage.js
@@ -17,7 +17,6 @@ function IndividualResourcePage() {
     <div>
       <Banner imageUrl="https://cdn.pixabay.com/photo/2017/03/25/03/29/cherry-tomatoes-2172700_1280.jpg" />
 
-
       <CategoryButtonGroup
         title="FOOD INSECURITY RESOURCES"
         names={categorNames}
@@ -26,7 +25,6 @@ function IndividualResourcePage() {
 
       <TextBlock text = {"For many students today, food insecurity is just a few missed paychecks away. A 2018 study by found that 36% of college students are experienceing hunger and lack of stable housing. Add in the fact that tuition rates are going up while financial aid is going down, and it’s obvious that most college students and their families are feeling a very tight financial squeeze. But there is help out there. Students struggling to avoid hunger can find several resources to put food on the table while still completing their education."}/>
 
-      <TextBlock text = {"For many students today, food insecurity is just a few missed paychecks away. A 2018 study by found that 36% of college students are experienceing hunger and lack of stable housing. Add in the fact that tuition rates are going up while financial aid is going down, and it’s obvious that most college students and their families are feeling a very tight financial squeeze. But there is help out there. Students struggling to avoid hunger can find several resources to put food on the table while still completing their education."}/>
       <IndividualResourceTileGroup
         id="School"
         title="School"
