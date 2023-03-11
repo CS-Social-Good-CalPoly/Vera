bd5a9baaa3ab2fc4bb9452228175c7ee4c67b707
plandsman11 <parker.rey@gmail.com>
2023-03-11 13:15:04 -0800

    Touched up individual story page

--- vera-react-app/src/components/StoryPopUp/StoryPopUp.js
+++ vera-react-app/src/components/StoryPopUp/StoryPopUp.js
@@ -2,32 +2,41 @@ import React, {useState} from 'react'
 import {Card} from 'react-bootstrap';
 import {ResourcePageTileGroup} from '../components.js'
 import styled from 'styled-components'
+import mockRelevantResourceData from './mockRelevantData.json';
 
 const Header = styled.div`
-    letter-spacing: 0.05em;
     font-size: 32px;
-    margin: 0 8px 0 8px;
 
     #category {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
-    }
 
-    @media only screen and (max-width: 768px) {
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: space-between;
-        padding-bottom: 10px;
+        @media only screen and (max-width: 768px) {
+            display: flex;
+            flex-direction: column;
+        }
     }
 `
 
+const StoryCategory = styled(Card.Title)`
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 72px;
+    text-transform: uppercase;
+    
+    @media only screen and (max-width: 768px) {
+        font-size: 32px;
+        line-height: 32px;
+    }    
+`
+
 const Year = styled(Card.Subtitle)`
-    margin-bottom: 7px;
     font-size: 32px;
+    padding-bottom: 8px;
 
     @media only screen and (max-width: 768px) {
-        font-size: 14px
+        font-size: 18px
     }
 `
 
@@ -35,60 +44,36 @@ const Major = styled(Card.Subtitle)`
     font-size: 32px;
 
     @media only screen and (max-width: 768px) {
-        font-size: 14px
-    }
-`
-
-const Body = styled(Card.Body)`
-    margin-bottom: 5%;
-    margin-left: 5%;
-    margin-right: 5%;
-    font-size: 32px;
-
-    @media only screen and (max-width: 768px) {
-        font-size: 14px
+        font-size: 18px
     }
 `
 
 const Date = styled(Card.Text)`
-    text-align: center;
     font-size: 32px;
+    padding-bottom: 8px;
 
     @media only screen and (max-width: 768px) {
-        font-size: 14px;
-        margin-top: -10px;
+        font-size: 18px;
     }
 `
 
 const Cardstory = styled(Card.Body)`
     padding: 0;
-    margin-top: 15%;
-    // margin-bottom: 17px;
+    margin-top: 80px;
 
     @media only screen and (max-width: 768px) {
         margin-bottom: 0;
-    }
-`
-
-const Storybody = styled.div`
-    border: 1px solid #4A6E82;
-    box-shadow: 4px 4px 15px rgba(114, 141, 149, 0.15);
-    border-radius: 30px;
-    padding: 25px;
-
-    @media only screen and (max-width: 768px) {
-        margin: -15px 0 15px 0;
+        margin-top: 40px;
     }
 `
 
 const Storytitle = styled(Card.Title)`
-    visibility: visible;
     font-size: 36px;
     font-weight: 600;
     text-transform: uppercase;
 
     @media only screen and (max-width: 768px) {
-        display: none;
+        font-size: 24px;
     }
 `
 
@@ -106,65 +91,32 @@ const PopupResources = styled.div`
 const CardWrapper = styled(Card)`
     font-family: 'Poppins';
     color: #4A6E82;
-    border-radius: 30px;
     max-width: 80%;
     margin: 5vh auto auto;
-`
-
-const DesktopHeader = styled(Card.Title)`
-    visibility: visible;
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 48px;
-    line-height: 72px;
+    box-shadow: 4px 4px 15px rgba(114, 141, 149, 0.15);
+    border-radius: 30px;
     letter-spacing: 0.05em;
-    text-transform: uppercase;
-    
-
-    @media only screen and (max-width: 768px) {
-        display: none;
-    }
-
-
-
-    
-    
 `
-
+const Body = styled(Card.Body)`
+    margin-bottom: 5%;
+    margin-left: 5%;
+    margin-right: 5%;
+`
 
 const Text = styled(Card.Text)`
-    // position: absolute;
-    // width: 926px;
-    // height: 429px;
-    // left: 87px;
-    // top: 111.59px;
-
-    font-family: 'Poppins';
-    font-style: normal;
     font-weight: 400;
-    font-size: 16px;
-    line-height: 35px;
-    /* or 219% */
-    letter-spacing: 0.05em;
+    font-size: 20px;
+    line-height: 42px;
 
-    @media only screen and (max-width: 768px) {    
-        display: revert;
-        font-size: 20px;
-    }
-`
-
-const Title = styled(Card.Title)`
-    display: none;
-
-    @media only screen and (max-width: 768px) {    
-        display: revert;
-        font-size: 20px;
+    @media only screen and (max-width: 768px) {
+        font-size: 16px;
+        line-height: 35px;
     }
 `
 
 function StoryPopUp(props) {
     const [size, setSize] = useState(false);
+    const relevantResourceData = mockRelevantResourceData;
 
     function change() {
         setSize(true);
@@ -180,12 +132,9 @@ function StoryPopUp(props) {
         <test>
         <CardWrapper hidden={size}>
             <Body>
-                {/* <Title style={{fontWeight: '600', paddingLeft: '10px'}} className='mobile'>
-                    How My Dog Helps me Through College
-                </Title> */}
                 <Header>
                     <div id='category'>
-                        <DesktopHeader>Family</DesktopHeader>
+                        <StoryCategory>Family</StoryCategory>
                         <Date>Dec 11, 2020</Date>
                     </div>
                     <div>
@@ -194,26 +143,21 @@ function StoryPopUp(props) {
                     </div>
                 </Header>
                 <Cardstory>
-                    {/* <Storybody style={{border: 'none'}}> */}
-                        <Storytitle>
-                        How My Dog Helps me Through College
-                        </Storytitle>
-                        <Text>
-                        I first began experiencing anxiety and depression at the age of 14 after being bullied at school for years. While at first it would come and go, anxiety and depression eventually became a constant presence in my life. It was like a perpetual cough that eventually starts to get better, only to come back worse than before. Only unlike a cough, where usually I am still able to function, anxiety and depression hits like a ton of bricks and even the idea of getting out of bed seems to be a goal that gets to be less and less attainable. As time passed, more and more of my days started to be spent paralyzed by endless thoughts of regrets of the past and worries for the future.
-    I was so intent on finding the solution to overcoming my anxiety and depression that I studied mental health in school, from college to grad school for 7 years, and yet still felt I hadn't even come close to grasping how to manage my own anxiety and depression. I felt very confident about helping others; yet, horribly useless at helping myself. Something was missing; a piece to my puzzle that I had yet to discover.
-                        </Text>
-                    {/* </Storybody> */}
+                    <Storytitle>
+                    How My Dog Helps me Through College
+                    </Storytitle>
+                    <Text>
+                    I first began experiencing anxiety and depression at the age of 14 after being bullied at school for years. While at first it would come and go, anxiety and depression eventually became a constant presence in my life. It was like a perpetual cough that eventually starts to get better, only to come back worse than before. Only unlike a cough, where usually I am still able to function, anxiety and depression hits like a ton of bricks and even the idea of getting out of bed seems to be a goal that gets to be less and less attainable. As time passed, more and more of my days started to be spent paralyzed by endless thoughts of regrets of the past and worries for the future.
+I was so intent on finding the solution to overcoming my anxiety and depression that I studied mental health in school, from college to grad school for 7 years, and yet still felt I hadn't even come close to grasping how to manage my own anxiety and depression. I felt very confident about helping others; yet, horribly useless at helping myself. Something was missing; a piece to my puzzle that I had yet to discover.
+                    </Text>
                 </Cardstory>
             </Body>
-
         </CardWrapper>
-        
-
             <PopupResources>
                 <ResourcePageTileGroup
                     id="RelevantResources"
                     title="Relevant Resources"
-                    resources={resources}
+                    resources={relevantResourceData}
                 />
             </PopupResources>
         </test>
