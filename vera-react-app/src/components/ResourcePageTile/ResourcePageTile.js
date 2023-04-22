ee052c947d06b43954fccadd8a6ba358fa133b2d
wbrandon25 <legobw1018@gmail.com>
2023-04-21 18:48:18 -0700

    add static ellipses

--- vera-react-app/src/components/ResourcePageTile/ResourcePageTile.js
+++ vera-react-app/src/components/ResourcePageTile/ResourcePageTile.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect, useRef}from "react";
 import styled from 'styled-components';
 import arrowIcon from '../Shared/arrow-icon.svg';
 import { Tile, TileIcon, TileTitle, TileBanner } from '../Shared/Tile';
 import { Link } from "react-router-dom";
 import '../Shared/Tile.css';
+// import './resourcePageTile.css';
 
 const InfoText = styled.p`
   font-family: Poppins;
@@ -22,6 +23,7 @@ const InfoText = styled.p`
   overflow: hidden;
   height: 100px;
   
+  
   @media only screen and (max-width: 768px) {
     font-size: 10px;
     line-height: 12px;
@@ -32,6 +34,8 @@ const InfoText = styled.p`
   }
 `;
 
+
+
 /* Component Props
  * imageUrl
  * title
@@ -39,16 +43,33 @@ const InfoText = styled.p`
  */
 
 function ResourcePageTile(props) {
+    const infoRef = useRef(null);
+  
+    // useEffect(() => {
+    //   const infoElement = infoRef.current;
+    //   if (infoElement.scrollHeight > infoElement.clientHeight) {
+    //     infoElement.classList.add('overflowed');
+    //   }
+    // }, []);
+    useEffect(() => {
+        setTimeout(() => {
+          const infoElement = infoRef.current;
+          if (infoElement.scrollHeight > infoElement.clientHeight) {
+            infoElement.classList.add('overflowed');
+          }
+        }, 1000);
+      }, []);
+  
     return (
       <Tile onClick={props.handleClick} className='tile'>
         <Link to="/individualResource" className='tile-link'>
           <TileBanner src={props.imageUrl} alt={props.title} />
           <TileTitle>{props.title}</TileTitle>
-          <InfoText>{props.infoText}</InfoText>
+          <InfoText ref={infoRef}>{props.infoText}</InfoText>
           <TileIcon src={arrowIcon} />
         </Link>
       </Tile>
-    )
-}
+    );
+  }
 
 export default ResourcePageTile;
