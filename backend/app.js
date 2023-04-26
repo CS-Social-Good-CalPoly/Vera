0bf0260c21d56dcc057d4c5a7f6002ef6fbfa111
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2023-04-25 18:22:21 -0700

    completed

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 require('dotenv').config()
 
 const app = express()
-const PORT = 5000
+const PORT = 3001
 const DB_CONNECTION = process.env.DB_CONNECTION
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const resourceRoutes = require('./routes/resources')
 const storyRoutes = require('./routes/stories')
