9e62e15f076177e227adcb9cfc345e2a97f19ee9
wbrandon25 <73367549+wbrandon25@users.noreply.github.com>
2023-05-11 23:42:54 -0700

    remove unused/redundant css + cleanup

--- vera-react-app/src/components/StorySubmission/StorySubmission.js
+++ vera-react-app/src/components/StorySubmission/StorySubmission.js
@@ -57,38 +57,21 @@ function StorySubmission() {
   
   return (
       <div>
-          <div class="some_container">
-              <div class="left-box">
-                  <div class="left-element">              <DropDownForm className="left-element" fieldTitle="Year" myoptions={yearList} handleChange={handleYearChange} />
-                  </div>
-                  <div class="left-element">                      <DropDownForm className="left-element" fieldTitle="College" myoptions={collegeList} handleChange={handleCollegeChange} />
-                  </div>
-              </div>
-              <div class="right-box"><DropDownOptionalForm className="left-element" fieldTitle="Major (optional)" myoptions={majorList} handleChange={handleMajorChange}/></div>
-          </div>
-
-
           <div className="background">
-            <form className="story-submission-box" onSubmit={verifySubmission}>
-              {/* <div className="story-submission-box"> */}
-                {/* <div className='some_container'>
-                  <div className="left-box">
-                      <DropDownForm className="left-element" fieldTitle="Year" myoptions={yearList} handleChange={handleYearChange}/> 
-                      <DropDownForm className="left-element" fieldTitle="College" myoptions={collegeList} handleChange={handleCollegeChange}/>
-                  </div>
-                  <div className="right-box" id="option">
-                      <DropDownOptionalForm className="left-element" fieldTitle="Major (optional)" myoptions={majorList} handleChange={handleMajorChange}/>
-                  </div>
-                </div> */}
-                  <div class="some_container">
-                      <div class="left-box">
-                          <div class="left-element"> <DropDownForm className="left-element" fieldTitle="Year" myoptions={yearList} handleChange={handleYearChange} />
+              <form className="story-submission-box" onSubmit={verifySubmission}>
+                  <div class="input-outer-container">
+                      <div class="inner-container-box">
+                          <div >
+                              <DropDownForm fieldTitle="Year" myoptions={yearList} handleChange={handleYearChange} />
                           </div>
-                          <div class="left-element"> <DropDownForm className="left-element" fieldTitle="College" myoptions={collegeList} handleChange={handleCollegeChange} />
+                          <div >
+                              <DropDownForm fieldTitle="College" myoptions={collegeList} handleChange={handleCollegeChange} />
                           </div>
                       </div>
-              <div class="right-box"><DropDownOptionalForm className="left-element" fieldTitle="Major (optional)" myoptions={majorList} handleChange={handleMajorChange}/></div>
-          </div>
+                      <div class="inner-container-box">
+                          <DropDownOptionalForm fieldTitle="Major (optional)" myoptions={majorList} handleChange={handleMajorChange} />
+                      </div>
+                  </div>
                 <div className="description-box"> 
                   <div className="title-text">
                     Short Description
