0d8e40c559f551e505ff428c07f17319100003e9
Jinrong Pettit <97060375+jinpettit@users.noreply.github.com>
2023-05-15 15:47:58 -0700

    Populate individual (#170)
    * populated individual story page
    
    * added missing route
    
    * fixed typeError and formatted date

--- vera-react-app/src/pages/IndividualStoryPage/IndividualStoryPage.js
+++ vera-react-app/src/pages/IndividualStoryPage/IndividualStoryPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Banner, StoryPopUp } from '../../components/components';
+import { StoryPopUp } from '../../components/components';
 
 function IndividualStoryPage() {
 
   return (
     <div>
-      <Banner imageUrl="https://cdn.pixabay.com/photo/2017/03/25/03/29/cherry-tomatoes-2172700_1280.jpg" />
       <StoryPopUp/>
     </div>
 
