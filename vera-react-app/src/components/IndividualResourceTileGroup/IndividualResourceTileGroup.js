d7bcaf27365d0e4d804d9e17644909a99366ce07
amanikonda123 <adityamanikonda123@gmail.com>
2023-05-16 03:38:55 -0700

    #163 populate indiv resource page, added index key

--- vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourceTileGroup.js
+++ vera-react-app/src/components/IndividualResourceTileGroup/IndividualResourceTileGroup.js
@@ -15,7 +15,7 @@ function IndividualResourceTileGroup({ id, title, resources }) {
       </TitleContainer>
       <TileGroup>
         {resources.map((resource, index) => (
-          <IndividualResourceTile
+          <IndividualResourceTile key={index}
             imgUrl={resource.imageUrl}
             title={resource.title}
             description={resource.description}
