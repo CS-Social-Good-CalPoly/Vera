5a1a399aab50d5f4c378bce4644305ff1b1d2a14
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2024-01-14 12:51:03 -0800

    changed cors line

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const app = express()
 const PORT = 3001
 const DB_CONNECTION = process.env.DB_CONNECTION
 
-app.use(cors({ origin: 'https://vera-backend.onrender.com' }))
+app.use(
+    cors({
+        origin: 'https://deployattempt3--tangerine-griffin-d9035a.netlify.app',
+    }),
+)
 
 const resourceRoutes = require('./routes/resources')
 const storyRoutes = require('./routes/stories')
