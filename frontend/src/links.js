250cabe22b33e5033f2f1ac18f30d5955df67a0e
Sean Nguyen <smnguyen745@gmail.com>
2024-01-21 17:24:47 -0800

    feat: cleaned frontend/src/links.js exports

--- frontend/src/links.js
+++ frontend/src/links.js
@@ -1,6 +1,6 @@
 const PROTOCOL = 'http'
 const HOSTNAME = 'localhost'
-export const PORT = '3001'
+const PORT = '3001'
 const URL_PATH = PROTOCOL + '://' + HOSTNAME + ':' + PORT
 
 export default URL_PATH
