603eb8d8fcfc452ce0c7259e8be03d679caaa136
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2024-01-29 17:50:58 -0800

    added netlify cors

--- frontend/src/links.js
+++ frontend/src/links.js
@@ -1,6 +1,6 @@
 const PROTOCOL = 'http'
 const HOSTNAME = 'localhost'
 const PORT = '3001'
-const URL_PATH = 'https://vera-backend.onrender.com'
+const URL_PATH = PROTOCOL + '://' + HOSTNAME + ':' + PORT
 
 export default URL_PATH
