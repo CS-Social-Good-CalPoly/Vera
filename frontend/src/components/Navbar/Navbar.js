cb19106e76c066d1b0dd2c0350ae8748cffc5d02
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2024-02-01 10:48:41 -0800

    fixed typo

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,34 +1,30 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import logo from './VeraLogo.jpg'
 
-
-
 function NavBar() {
-
     const [activeLink1, setActiveLink1] = useState(true)
     const [activeLink2, setActiveLink2] = useState(false)
     const [activeLink3, setActiveLink3] = useState(false)
 
-     const handleLinkClick1 = () => {
-         setActiveLink1(true)
-         setActiveLink2(false)
-         setActiveLink3(false)
-     }
-     const handleLinkClick2 = () => {
-         setActiveLink2(true)
-         setActiveLink1(false)
-         setActiveLink3(false)
-     }
-     const handleLinkClick3 = () => {
-
-         setActiveLink3(true)
-         setActiveLink2(false)
-         setActiveLink1(false)
-     }
+    const handleLinkClick1 = () => {
+        setActiveLink1(true)
+        setActiveLink2(false)
+        setActiveLink3(false)
+    }
+    const handleLinkClick2 = () => {
+        setActiveLink2(true)
+        setActiveLink1(false)
+        setActiveLink3(false)
+    }
+    const handleLinkClick3 = () => {
+        setActiveLink3(true)
+        setActiveLink2(false)
+        setActiveLink1(false)
+    }
 
     return (
         <Navbar className="nav-main" bg="light" expand="lg">
@@ -41,7 +37,7 @@ function NavBar() {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     <Link
-                        nClick={handleLinkClick1}
+                        onClick={handleLinkClick1}
                         style={{
                             'text-decoration': activeLink1 ? 'underline' : '',
                         }}
@@ -63,9 +59,7 @@ function NavBar() {
                     <Link
                         onClick={handleLinkClick3}
                         style={{
-                            'text-decoration': activeLink3
-                                ? 'underline'
-                                : '',
+                            'text-decoration': activeLink3 ? 'underline' : '',
                         }}
                         className="navbar-links"
                         to="/StorySubmission"
