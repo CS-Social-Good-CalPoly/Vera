6e70dfa57d99e8218459a08f63f54a3460836bdd
Parshana007 <parshana007@gmail.com>
2024-02-01 14:12:46 -0800

    fixed with prettier

--- frontend/src/components/StorySubmission/StorySubmission.js
+++ frontend/src/components/StorySubmission/StorySubmission.js
@@ -1,83 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { DropDownForm, DropDownOptionalForm } from '../components';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import './StorySubmission.css';
-import axios from 'axios';
-import cheerio from 'cheerio';
+import React, { useState, useEffect } from 'react'
+import { DropDownForm, DropDownOptionalForm } from '../components'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import './StorySubmission.css'
+import axios from 'axios'
+import cheerio from 'cheerio'
 // const cheerio = require('cheerio');
 // const scrapeWebsite = require('./scrapeWebsite.cjs');
 
 function StorySubmission() {
-    const [year, setYear] = useState('');
-    const [college, setCollege] = useState('');
-    const [major, setMajor] = useState('');
-    const [quillValue, setQuillValue] = useState('');
-    const [title, setTitleValue] = useState('');
-    const [collegeList, setCollegeList] = useState([]);
-    const [majorList, setMajorList] = useState([]);
+    const [year, setYear] = useState('')
+    const [college, setCollege] = useState('')
+    const [major, setMajor] = useState('')
+    const [quillValue, setQuillValue] = useState('')
+    const [title, setTitleValue] = useState('')
+    const [collegeList, setCollegeList] = useState([])
+    const [majorList, setMajorList] = useState([])
 
     const values = {
         Year: year,
         College: college,
         Major: major,
         Description: quillValue,
-    };
+    }
 
     const handleTitleKeyPress = (e) => {
-        setTitleValue(e.target.value);
-    };
+        setTitleValue(e.target.value)
+    }
 
     const handleYearChange = (e) => {
-        setYear(e);
-    };
+        setYear(e)
+    }
 
     const handleCollegeChange = (e) => {
-        setCollege(e);
-    };
+        setCollege(e)
+    }
 
     const handleTitleChange = (e) => {
-        setTitleValue(e.target.value);
-    };
+        setTitleValue(e.target.value)
+    }
 
     const handleMajorChange = (e) => {
-        setMajor(e);
-    };
+        setMajor(e)
+    }
 
     useEffect(() => {
-    axios.get('https://www.calpoly.edu/colleges-departments-and-majors')
-    .then(res => {
-    const $ = cheerio.load(res.data);
-    const college_lst = [];
-    const major_lst = [];
+        axios
+            .get('https://www.calpoly.edu/colleges-departments-and-majors')
+            .then((res) => {
+                const $ = cheerio.load(res.data)
+                const college_lst = []
+                const major_lst = []
 
-    // Select each h2 tag
-    $('h2').each((index, element) => {
-        // Get the text content of the h2 tag
-        const h2Text = $(element).text();
-        
-        // Check if the text content contains the word "college"
-        if (h2Text.toLowerCase().includes('college')) {
-            college_lst.push(h2Text);
-        }
-    });
-    setCollegeList(college_lst);
+                // Select each h2 tag
+                $('h2').each((index, element) => {
+                    // Get the text content of the h2 tag
+                    const h2Text = $(element).text()
 
-    $('a').each((index, element) => {
-        // Get the text content of the h2 tag
-        const aText = $(element).text();
-        
-        // Check if the text content contains the word "college"
-        if (aText.toLowerCase().includes('major') && aText !== "Find a major") {
-            console.log(aText);
-            major_lst.push(aText);
-        }
-    });
-    setMajorList(major_lst);
+                    // Check if the text content contains the word "college"
+                    if (h2Text.toLowerCase().includes('college')) {
+                        college_lst.push(h2Text)
+                    }
+                })
+                setCollegeList(college_lst)
 
-    })
-    .catch(err => console.error(err));
-    }, []);
+                $('a').each((index, element) => {
+                    // Get the text content of the h2 tag
+                    const aText = $(element).text()
+
+                    // Check if the text content contains the word "college"
+                    if (
+                        aText.toLowerCase().includes('major') &&
+                        aText !== 'Find a major'
+                    ) {
+                        console.log(aText)
+                        major_lst.push(aText)
+                    }
+                })
+                setMajorList(major_lst)
+            })
+            .catch((err) => console.error(err))
+    }, [])
 
     const yearList = [
         '1st Year',
@@ -85,25 +88,25 @@ function StorySubmission() {
         '3rd Year',
         '4th Year',
         '5th+ Year',
-    ];
+    ]
 
     function verifySubmission(e) {
         // if an option is selected, the value is stored as 1 at the moment
     }
 
     async function handlePost(e) {
-        e.preventDefault();
+        e.preventDefault()
         if (
             year === '' ||
             college === '' ||
             quillValue === '' ||
             title === ''
         ) {
-            alert('Complete missing fields');
-            console.log('Missing info');
-            return;
+            alert('Complete missing fields')
+            console.log('Missing info')
+            return
         }
-    
+
         try {
             const data = {
                 Title: 'My Story Title',
@@ -112,25 +115,27 @@ function StorySubmission() {
                 StudentMajor: values.Major,
                 StudentCollege: values.College,
                 StudentYear: values.Year,
-            };
-    
-            const response = await fetch('http://localhost:3001/stories/storysubmission', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
+            }
+
+            const response = await fetch(
+                'http://localhost:3001/stories/storysubmission',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
                 },
-                body: JSON.stringify(data),
-            });
-    
-            const responseData = await response.json();
-            console.log('Server response:', responseData);
-            alert('Thank you for your submission!');
+            )
+
+            const responseData = await response.json()
+            console.log('Server response:', responseData)
+            alert('Thank you for your submission!')
         } catch (err) {
-            console.error('Error submitting data:', err);
-            alert('Error submitting data. Please try again later.');
+            console.error('Error submitting data:', err)
+            alert('Error submitting data. Please try again later.')
         }
     }
-    
 
     return (
         <div>
@@ -200,4 +205,4 @@ function StorySubmission() {
     )
 }
 
-export default StorySubmission;
+export default StorySubmission
