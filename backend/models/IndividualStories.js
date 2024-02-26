356d0e0022bb8d201ab81ed0cd00120171886806
Matteo Terrien <matteo.terrien@gmail.com>
2024-02-26 11:37:29 -0800

    Created a token on the frontend and got it to display on story submission. Still working on getting into the backend and checking to see if it's unique

--- backend/models/IndividualStories.js
+++ backend/models/IndividualStories.js
@@ -16,8 +16,19 @@ const StorySchema = new Schema({
     StudentCollege: String,
     StudentYear: String,
     GeneralCategory: String,
+    Token: String,
     Status: { type: String, default: 'review' },
     RejectionReasonList: { type: [String], default: [] },
 })
+
+StorySchema.statics.tokenExists = async function (token) {
+    try {
+        const existingToken = await this.findOne({ Token: token })
+        return !!existingToken
+    } catch (error) {
+        console.error('Error checking token:', error)
+        throw error
+    }
+}
 
 module.exports = model('story', StorySchema, 'story')
