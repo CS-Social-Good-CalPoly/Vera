b8a9514ae387539258cd7d16e3ced409190d595f
eliseemelia <eliseemelia@gmail.com>
2024-05-01 16:54:33 -0700

    added landing page

---
+++ frontend/src/components/LandingBanner/LandingBanner.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './LandingBanner.css'
+import { LinkButton } from '../components'
+
+function LandingBanner(props) {
+    return (
+        <div
+            className="img"
+            style={{
+                backgroundImage: `url(${props.imageUrl})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
+            <div className="textCol">
+                <h1 className="header1">{props.pageTitle}</h1>
+                <div className='banner-button-group'>
+                    <LinkButton 
+                        link='/resources'
+                        title='RESOURCES'
+                        color='white'
+                        background='#728D95'
+                    />
+                    <LinkButton 
+                    link='/stories'
+                    title='STORIES'
+                    color='white'
+                    background='#728D95'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingBanner
---
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,59 @@
+import './LandingPage.css'
+import cp from '../../components/LandingBanner/calpoly.jpg'
+import resource_img from '../../components/PhotoBanner/plant_hand.jpg'
+import stories from '../../components/PhotoBanner/stories.jpg'
+
+import { 
+    LandingBanner,
+    PhotoBanner,
+    TextBanner
+} from '../../components/components'
+import '../../links'
+
+function LandingPage(){
+
+  return (
+    <div className='landing-page'>
+      <LandingBanner
+      imageUrl={cp}
+      pageTitle="A Student-driven Hub for Mental Wellness."
+      />
+      <TextBanner 
+        background='#48858D'
+        color='white'
+        title='Our Mission'
+        bodyText="We're dedicated to improving accessibility by providing a comprehensive directory of campus resources, ensuring that every student can easily access the support they need. Further, we aim to foster a culture of openness and understanding through our shared stories platform to ultimately break the stigma around mental health."
+        buttonTitles= {['LEARN MORE','STORIES']}
+        buttonLinks={{'LEARN MORE':'/resources', 'STORIES':'/stories'}}
+        buttonColor='#48858D'
+        buttonBackground='white'
+      />
+      <PhotoBanner 
+        background='#728D95'
+        color='black'
+        title='RESOURCES'
+        bodyText="Discover the diverse array of resources readily available through Cal Poly, providing you with up-to-date and valuable information for your needs."
+        buttonTitles= {['RESOURCES']}
+        buttonLinks={{'RESOURCES':'/resources'}}
+        buttonColor='black'
+        buttonBackground='#48858D'
+        imageUrl={resource_img}
+        bannerOrder={['text', 'photo']}
+      />
+      <PhotoBanner 
+        background='#DCE3E7'
+        color='#48858D'
+        title='STORIES'
+        bodyText="Dive into a collection of inspiring experiences, empowering users to contribute their stories and find solace in the shared narratives of others."
+        buttonTitles= {['STORIES', 'SHARE YOUR STORY']}
+        buttonLinks={{'STORIES':'/stories', 'SHARE YOUR STORY':'/storySubmission'}}
+        buttonColor='#48858D'
+        buttonBackground='white'
+        imageUrl={stories}
+        bannerOrder={['photo','text']}
+      />
+    </div>
+  )
+}
+
+export default LandingPage
