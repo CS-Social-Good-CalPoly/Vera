c7c5e812b487b961fe04252e965e4c4b897036f6
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-06-03 09:02:15 -0700

    remove titlecontainer

--- frontend/src/components/StoryTileGroup/StoryTileGroup.js
+++ frontend/src/components/StoryTileGroup/StoryTileGroup.js
@@ -11,9 +11,6 @@ import { StoryTile } from '../components'
 function StoryTileGroup({ id, title, stories }) {
     return (
         <TileGroupDiv>
-            <TitleContainer>
-                <Heading id={id}>{title}</Heading>
-            </TitleContainer>
             <TileGroup>
                 {stories.map((story, index) => {
                     return (
