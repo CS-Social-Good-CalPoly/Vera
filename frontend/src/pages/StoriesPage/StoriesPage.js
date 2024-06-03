876c54d7368672fbea559b108b1e9a31d4a33b67
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-06-03 09:02:56 -0700

    remove categorybuttongroup

--- frontend/src/pages/StoriesPage/StoriesPage.js
+++ frontend/src/pages/StoriesPage/StoriesPage.js
@@ -57,11 +57,11 @@ function StoriesPage({ setActiveLink }) {
                 imageUrl="https://images.unsplash.com/photo-1506962240359-bd03fbba0e3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2065&q=80"
                 displayButton="true"
             />
-            <CategoryButtonGroup
+            {/* <CategoryButtonGroup
                 title="Categories"
                 names={categorNames}
                 locations={categorNames}
-            />
+            /> */}
             <DropDownForm
                 fieldTitle="Categories"
                 myoptions={categorNames}
