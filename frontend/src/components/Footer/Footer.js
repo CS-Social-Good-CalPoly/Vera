f6e78fc4b229a6dc7ce81fb08970b72e84e902f6
Emily <ewong128@calpoly.edu>
2025-01-22 18:27:20 -0800

    Added phone numbers to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,7 +5,15 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <footer className="main-footer">
-            <div className="column1">&copy; 2024 VERA</div>
+               
+            <div className="column5">Call or Text
+                <br></br>
+                National Suicide Hotline: 1 (800)-273-3255 or 988
+          <br></br>
+             Cal Poly Police: (805)-783-2281
+             </div>
+         <div className="column1">&copy; 2024 VERA</div>
+            
             <div className="column3">
                 
                 <Link
