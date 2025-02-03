7eb918635be52431b08dec7518108da1e96d03e4
briancsp <runnerb2003@gmail.com>
2025-02-02 20:56:02 -0800

    correct file name

--- backend/routes/web_scraping.js
+++ backend/routes/web_scraping.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const router = express.Router()
 const IndResources = require('../models/IndividualResources')
