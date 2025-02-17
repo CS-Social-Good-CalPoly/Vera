b75862b6220a8c284f052a4d84285276701d3ce6
Emily <ewong128@calpoly.edu>
2025-02-16 22:22:41 -0800

    Made some updates to the scraping

---
+++ backend/models/Treatmentrecovery.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require('mongoose')
+
+const TreatmentrecoverySchema = new Schema({
+    Title: String,
+    ImageUrl: String,
+    ImageAltText: String,
+    // RelevantCategoryList: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'general-story-category',
+    //     },
+    // ],
+    ParagraphText: String,
+    Date: Date,
+    // StudentMajor: String,
+    // StudentCollege: String,
+    // StudentYear: String,
+    // GeneralCategory: String,
+    // Token: String,
+    // Approved: { type: Boolean, default: false },
+    // Status: { type: String, default: 'review' },
+    // RejectionReasonList: { type: [String], default: [] },
+})
+
+module.exports = model('treatment', TreatmentrecoverySchema, 'treatment')
