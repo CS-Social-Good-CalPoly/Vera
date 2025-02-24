711aedf0e1aa13b6f6c550ed4dd18375dcf97b80
Emily <ewong128@calpoly.edu>
2025-02-24 14:07:11 -0800

    Added the cal poly health and hyperlinked the phone nunmbers

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,15 +5,24 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <footer className="main-footer">
-            <div className="column5">Call or Text
-            <br></br>
-                National Suicide Hotline: 1 (800)-273-8255 or 988
-          <br></br>
-             Cal Poly Police: (805)-756-2281
-             </div>
-         <div className="column1">&copy; 2024 VERA</div>
+            <div className="column5">
+                Call or Text
+                <br></br>
+                <div class="inline-container">
+                <span class="inline-text">National Sucide Hotline:</span> 
+                <a href="tel:+18002738255">+1(800)-273-8255</a>or<a href="tel:+988">988</a>
+                </div>
+                <div class="inline-container">
+                <span class="inline-text">Cal Poly Health:</span> 
+                <a class="inline-text" href="tel:+18057562511">+1 (805)-756-2511</a>
+                </div>
+                <div class="inline-container">
+                <span class="inline-text">Cal Poly Police:</span> 
+                <a class="inline-text"href="tel:+18057562281">+1 (805)-756-2281</a> 
+                </div>
+            </div>
+            <div className="column1">&copy; 2024 VERA</div>
             <div className="column3">
-                
                 <Link
                     onClick={() => {
                         window.scroll(0, 0)
@@ -35,11 +44,11 @@ function Footer() {
                     Stories{' '}
                 </Link>
                 <Link
-                    onClick = {() => {
+                    onClick={() => {
                         window.scroll(0, 0)
                     }}
-                    className = "footer-links"
-                    to = "/StorySubmission"
+                    className="footer-links"
+                    to="/StorySubmission"
                 >
                     {' '}
                     Share Your Story{' '}
