af7a563cbbae9dcd4159f3bdea2269329291b54b
briancsp <runnerb2003@gmail.com>
2025-04-20 11:02:18 -0700

    create backend route to specific individual resource

--- backend/routes/resources.js
+++ backend/routes/resources.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const GenResources = require('../models/GenResources')
 const SubResources = require('../models/SubResources')
 const IndResources = require('../models/IndividualResources')
+const ObjectId = require('mongodb').ObjectId;
 
 // GET route for all general resource categories
 router.get('/generalrsrcscat', async (req, res) => {
@@ -41,6 +42,38 @@ router.get('/individualResources', async (req, res) => {
             resources = await IndResources.find()
         }
         res.json(resources)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// GET route for all stories associated with a specific token
+router.get('/Resources', async (req, res) => {
+    try {
+        // find resourceId from request
+        const resourceId = req.query.resourceId;
+
+        // check if the ID is provided and not empty
+        if (!resourceId) {
+            return res.status(400).json({ error: 'Missing or empty ID parameter' });
+        }
+
+        // validate the ID format
+        if (!ObjectId.isValid(resourceId)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
+        // convert to ObjectId
+        const resourceObjectId = ObjectId.createFromHexString(resourceId)
+
+        // get stories with IDs that belong to that token
+        const resource = await IndResources.findOne(
+            { _id: resourceObjectId }
+        )
+        if (!resource) {
+            return res.status(500).json({ message: `No resource with id ${resourceId} was found` })
+        }
+        res.status(200).json(resource)
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
