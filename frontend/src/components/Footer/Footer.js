474934ff3746735785d7ce0f817cf4f906b06a8f
Akshat K <akshatk1001@gmail.com>
2025-04-22 00:56:33 -0700

    Refactor Footer and StorySubmission components for consistent className usage

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -8,20 +8,28 @@ function Footer({ activeLink }) {
             <div className="column5">
                 Call or Text
                 <br></br>
-                <div class="inline-container">
-                <span class="inline-text">National Sucide Hotline:</span> 
-                <a href="tel:+18002738255">+1(800)-273-8255</a>or<a href="tel:+988">988</a>
+                <div className="inline-container">
+                    <span className="inline-text">
+                        National Sucide Hotline:
+                    </span>
+                    <a href="tel:+18002738255">+1(800)-273-8255</a>or
+                    <a href="tel:+988">988</a>
                 </div>
-                <div class="inline-container">
-                <span class="inline-text">Cal Poly Health:</span> 
-                <a class="inline-text" href="tel:+18057562511">+1 (805)-756-2511</a>
+                <div className="inline-container">
+                    <span className="inline-text">Cal Poly Health:</span>
+                    <a className="inline-text" href="tel:+18057562511">
+                        +1 (805)-756-2511
+                    </a>
                 </div>
-                <div class="inline-container">
-                <span class="inline-text">Cal Poly Police:</span> 
-                <a class="inline-text"href="tel:+18057562281">+1 (805)-756-2281</a> 
+                <div className="inline-container">
+                    <span className="inline-text">Cal Poly Police:</span>
+                    <a className="inline-text" href="tel:+18057562281">
+                        +1 (805)-756-2281
+                    </a>
                 </div>
             </div>
             <div className="column1">&copy; 2024 VERA</div>
+
             <div className="column3">
                 <Link
                     onClick={() => {
