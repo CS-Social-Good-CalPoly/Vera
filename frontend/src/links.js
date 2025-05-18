9760313ab3eb6813900fc0daa1df2400c57bd74c
Akshat K <akshatk1001@gmail.com>
2025-05-18 01:35:56 -0700

    Update URL_PATH to use production backend URL and comment out localhost configuration

--- frontend/src/links.js
+++ frontend/src/links.js
@@ -1,8 +1,8 @@
 const PROTOCOL = 'http'
 const HOSTNAME = 'localhost'
 const PORT = '3001'
-// const URL_PATH = 'https://vera-backend.onrender.com'
+const URL_PATH = 'https://vera-backend.onrender.com'
 
 // Uncomment this when you are testing on localhost
-const URL_PATH = `${PROTOCOL}://${HOSTNAME}:${PORT}`
+// const URL_PATH = `${PROTOCOL}://${HOSTNAME}:${PORT}`
 export default URL_PATH
