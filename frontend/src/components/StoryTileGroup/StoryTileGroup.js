11ba8d617a139d589fb18f2ffd82a5249ce44243
Sean Nguyen <smnguyen745@gmail.com>
2025-05-25 16:48:57 -0700

    Redsigned Stories page to match figma

--- frontend/src/components/StoryTileGroup/StoryTileGroup.js
+++ frontend/src/components/StoryTileGroup/StoryTileGroup.js
@@ -1,12 +1,61 @@
 import React from 'react'
-import {
-    TileGroupDiv,
-    TitleContainer,
-    Heading,
-    TileGroup,
-} from '../Shared/TileGroup.js'
-
 import { StoryTile } from '../components.js'
+import styled from 'styled-components'
+
+export const TileGroupDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const TitleContainer = styled.div`
+    border-bottom: 2px solid #000000;
+    display: inline-block;
+    width: 90%;
+    min-width: 430px;
+    margin: 0 5%;
+    align-self: flex-start;
+
+    @media only screen and (max-width: 768px) {
+        border-bottom: 2px solid black;
+        min-width: 100px;
+    }
+
+    @media only screen and (max-width: 360px) {
+        text-align: center;
+        align-self: center;
+    }
+`
+
+export const TileGroup = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+
+    @media only screen and (max-width: 768px) {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0 0 20px -8px;
+    }
+
+    @media only screen and (max-width: 360px) {
+        justify-content: center;
+    }
+`
+
+export const Heading = styled.h1`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 30px;
+    letter-spacing: 0.05em;
+    text-transform: capitalize;
+    padding-top: 15px;
+    padding-bottom: 5px;
+
+    @media only screen and (max-width: 768px) {
+        font-size: 20px;
+        line-height: 10px;
+    }
+`
 
 function StoryTileGroup({ id, title, stories, tokenStories }) {
     return (
@@ -16,8 +65,6 @@ function StoryTileGroup({ id, title, stories, tokenStories }) {
             </TitleContainer>
             <TileGroup>
                 {stories.map((story, index) => {
-                    console.log(tokenStories)
-                    console.log(tokenStories.includes(story._id))
                     return (
                         <StoryTile
                             key={index}
@@ -43,12 +90,6 @@ function StoryTileGroup({ id, title, stories, tokenStories }) {
                             }
                             categories={story.RelevantCategoryList}
                             editable={tokenStories.includes(story._id)}
-                            // buildingName={story.building}
-                            // address={story.address}
-                            // toExpect={story.whatToExpectList}
-                            // phone={story.phone}
-                            // hours={story.hourList}
-                            // link={story.resourceLink}
                         />
                     )
                 })}
