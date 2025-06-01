266b4a82ac1d7f84b4d0e110da73d2f8f0103774
Matteo T <89539137+matteoterrien@users.noreply.github.com>
2025-06-01 15:22:56 -0700

    removed box shadowing

--- frontend/src/components/Shared/Tile.js
+++ frontend/src/components/Shared/Tile.js
@@ -12,7 +12,6 @@ export const Tile = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
-    box-shadow: 0 2px 4px #344d5b;
 
     /* Hover effect */
     transition: transform 0.3s ease;
